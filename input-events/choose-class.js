759cfc088133ae5477f5b76b91354d250d658ac8
greg stadermann <greg.stadermann@sonnysdirect.com>
2022-10-08 21:58:24 -0400

    bundle naming changes

--- input-events/choose-class.js
+++ input-events/choose-class.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { Broadcast, EventUtil } = require('ranvier');
-const PlayerClass = require('../../bundle-example-classes/lib/PlayerClass');
+const PlayerClass = require('../../classes/lib/PlayerClass');
 
 /**
  * Player class selection event
