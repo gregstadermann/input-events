252efcd57c3fa1eec4e80376ee354a23b986ca98
greg stadermann <22124771+gregstadermann@users.noreply.github.com>
2024-03-21 13:48:26 -0400

    Race bonuses correctly calculated and added

--- input-events/finish-player.js
+++ input-events/finish-player.js
@@ -2,10 +2,14 @@
 
 const { Config, Player } = require('ranvier');
 const { PlayerClass } = require("../../classes/lib/PlayerClass");
+const PlayerRace = require('../lib/PlayerRace');
+
 
 /**
  * Finish player creation. Add the character to the account then add the player
  * to the game world
+ *
+ * @param {string} args.name
  */
 module.exports = {
   event: state => {
@@ -26,11 +30,14 @@ module.exports = {
           skills: args.skills,
           manaStat: args.manaStat,
           tps: args.tps,
+          primeStats: args.primeStats
       });
         console.log('args from finish-player', args);
         let skillMap = new Map(args.skills.map(obj => [obj.skill, obj.ranks]));
         let manaStat = args.stats.get(args.manaStat);
-        let primeStats = args.stats.get(args.primeStats);
+        let races = PlayerRace.getRaces();
+        let baseHealthPoints = PlayerRace.getRace(args.playerRace).baseHp;
+
         // TIP:DefaultAttributes: This is where you can change the default attributes for players
         // Skills are in number of ranks
         // All I need to do is get skills out of args.skills into this object which is written to the player
@@ -75,7 +82,7 @@ module.exports = {
             paladin_base: skillMap.get('paladin_base') || 0,
 
             mana: Math.round(manaStat/2),
-            health: Math.round((args.stats.get('constitution') + args.stats.get('strength')) / 10),
+            health: baseHealthPoints + Math.round((args.stats.get('constitution') + args.stats.get('strength')) / 10),
 
             discipline: args.stats.get('discipline'),
             aura: args.stats.get('aura'),
@@ -111,6 +118,7 @@ module.exports = {
         player.sex = args.sex;
         player.race = args.playerRace;
         player.tps = args.tps;
+        player.class = args.playerClass;
 
         const room = state.RoomManager.getRoom(startingRoomRef);
         player.room = room;
